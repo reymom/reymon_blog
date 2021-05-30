beeab0d921b7dfa63ada0b584fc0fdd59b716727
Reymom <ramon_gs95@hotmail.com>
2021-05-30 21:35:34 +0200

    add migrations

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,11 +2,11 @@
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
-    
+
     while ! nc -z $DB_HOST $DB_PORT; do
-      sleep 0.1
+        sleep 0.1
     done
-    
+
     echo "PostgreSQL started"
 fi
 
